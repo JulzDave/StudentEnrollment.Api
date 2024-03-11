5387d9cc8298d9154d0b0da1352d3b2282ad7554
Julian Dave Shikui <juliandave1989@gmail.com>
2024-03-11 02:03:52 +0200

    fix StudentEnrollment.Data

--- StudentEnrollment.Data/Enrollment.cs
+++ StudentEnrollment.Data/Enrollment.cs
@@ -5,5 +5,5 @@ public class Enrollment : BaseEntity
     public int CourseId { get; set; }
     public int StudentId { get; set; }
     public virtual Course Course { get; set; }
-    public virtual Course Student { get; set; }
+    public virtual Student Student { get; set; }
 }
